799f322d25fab4ea40537706727b6c143a863ec5
Robert Novotny <novotnr0@gmail.com>
2024-04-29 00:50:08 +0200

    Convert to Kotlin
    Rename .java to .kt

---
+++ src/main/java/com/github/novotnyr/mavenversion/MavenVersionProjectViewDecorator.kt
@@ -0,0 +1,41 @@
+package com.github.novotnyr.mavenversion
+
+import com.intellij.ide.projectView.PresentationData
+import com.intellij.ide.projectView.ProjectViewNode
+import com.intellij.ide.projectView.ProjectViewNodeDecorator
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.SimpleTextAttributes
+import org.jetbrains.idea.maven.project.MavenProject
+import org.jetbrains.idea.maven.project.MavenProjectsManager
+
+class MavenVersionProjectViewDecorator : ProjectViewNodeDecorator {
+    override fun decorate(node: ProjectViewNode<*>, data: PresentationData) {
+        if (!isDecorating) return
+        val nodeProject = node.project ?: return
+        val projectManager = MavenProjectsManager.getInstance(nodeProject) ?: return
+        val pomXml = node.pomXml ?: return
+        val mavenProject = projectManager.findProject(pomXml)
+        mavenProject?.version?.let {
+            data.version = it
+        }
+    }
+
+    private val isDecorating: Boolean
+        get() = PluginSettings.getInstance().isShowVersion
+
+    private var PresentationData.version: String
+        get() = ""
+        set(value) {
+            val separator = if (SystemInfo.isWindows) "  " else "\t"
+            addText(
+                separator + value, SimpleTextAttributes.GRAY_ATTRIBUTES
+            )
+        }
+
+    private val MavenProject.version: String?
+        get() = mavenId.version
+
+    private val ProjectViewNode<*>.pomXml: VirtualFile?
+        get() = virtualFile?.findChild("pom.xml")
+}
