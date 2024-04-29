1ce2562333b740563f552c51458dd59c0f4df644
Robert Novotny <novotnr0@gmail.com>
2024-04-29 14:43:17 +0200

    Use SNAPSHOT of IntelliJ Platform Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
-    id("org.jetbrains.intellij.platform") version "2.0.0-beta1"
+    id("org.jetbrains.intellij.platform") version "2.0.0-SNAPSHOT"
 }
 
 group = "com.github.novotnyr"
